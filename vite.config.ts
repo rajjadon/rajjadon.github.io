a84ea992fd5d530215c1739f3bd6d37275480f9c
Raj Jadon <rajjadon@gofynd.com>
2025-05-27 17:18:33 +0530

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx']
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        format: 'es',
-        chunkFileNames: '[name]-[hash].js',
-        entryFileNames: '[name]-[hash].js'
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  }
+  base: '/',
+  plugins: [react()]
 });
