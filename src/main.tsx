2fb9abd261851e034922a9a628d36a1d3086c3e5
rajjadon <rajjadon@gofynd.com>
2025-06-05 08:06:28 +0530

    feat: integrate copilotlive react sdk
    Add CopilotLive React SDK as a dependency and wrap the app with CopilotProvider. This enables AI-powered features in the application.

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme';
+import { CopilotProvider } from '@copilotlive/react-sdk';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <CopilotProvider token='cat-6r8kty8r'>
+        <App />
+      </CopilotProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
